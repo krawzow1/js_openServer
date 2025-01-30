3acb2726ef5fcfaf77daa232f36b0c77e341fdab
Ilya Kravtsov <krawzow1@yandex.ru>
2025-01-30 22:19:00 +0300

    калькулятор ккал с сохр знач в локалсторедж

--- js/script.js
+++ js/script.js
@@ -435,12 +435,17 @@ document.addEventListener('DOMContentLoaded', () => {
             dots.push(dot)
     }
 
+    function noDigits(n) {
+        return +n.replace(/\D/g, '')
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0,-2) * (slides.length - 1)) {
+        if (offset == noDigits(width) * (slides.length - 1)) {
             offset = 0
         } else {
-            offset += +width.slice(0,-2) 
+            offset += noDigits(width)
         }
+
         slidesField.style.transform = `translateX(-${offset}px)`
 
         if (slideIndex == slides.length) {
@@ -461,10 +466,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0,-2) * (slides.length - 1)
+            offset = noDigits(width) * (slides.length - 1)
         } else {
-            offset -= +width.slice(0,-2) 
+            offset -= noDigits(width) 
         }
+
         slidesField.style.transform = `translateX(-${offset}px)`
 
 
@@ -488,8 +494,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 const slideTo = e.target.getAttribute('data-slide-to')
                 
                 slideIndex = slideTo
-                offset = +width.slice(0,-2) * (slideTo - 1)
+                offset = noDigits(width) * (slideTo - 1)
                 slidesField.style.transform = `translateX(-${offset}px)`
+
 
 
                 if (slides.length < 10 ) {
@@ -548,19 +555,134 @@ document.addEventListener('DOMContentLoaded', () => {
     // })
     
 
-    localStorage.setItem('count', 1)
-    console.log(localStorage.getItem('count'))
 
-    const textInput = document.querySelector('#weight')
-    const localSave = localStorage.getItem('userTextInput')
+    // // Работа с localStorage
+    // localStorage.setItem('count', 1)
+    // console.log(localStorage.getItem('count'))
 
-    if (localSave) {
-        textInput.value = localSave
+    // const textInput = document.querySelector('#weight')
+    // const localSave = localStorage.getItem('userTextInput')
+
+    // if (localSave) {
+    //     textInput.value = localSave
+    // }
+
+    // textInput.addEventListener('input', () => {
+    //     localStorage.setItem('userTextInput', textInput.value)
+    // })
+    // console.log(document.querySelector('.calculating__choose-item'))
+    
+
+
+    //Калькулятор
+    const result = document.querySelector('.calculating__result span')
+    let sex, height, weight, age, ratio;
+    
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex')
+    } else {
+        sex = 'female'
+        localStorage.setItem('sex', sex)
+    }
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio')
+    } else {
+        ratio = 1.375
+        localStorage.setItem('ratio', ratio)
     }
 
-    textInput.addEventListener('input', () => {
-        localStorage.setItem('userTextInput', textInput.value)
-    })
-    console.log(document.querySelector('.calculating__choose-item'))
+    function initLocalSettings(selector, activeClass) {
+        const elements = document.querySelectorAll(selector)
+
+        elements.forEach(element => {
+            element.classList.remove(activeClass)
+            if (element.getAttribute('id') === localStorage.getItem('sex')) {
+                element.classList.add(activeClass)
+            }
+            if (element.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                element.classList.add(activeClass)
+            }
+        })
+
+    }
+
+    initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active')
+    initLocalSettings('#gender div', 'calculating__choose-item_active')
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '___'
+            return
+        }
+        
+        if (sex === 'female') {
+            result.textContent = Math.round(447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age) * ratio)
+        } else {
+            result.textContent = Math.round(88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age) * ratio)
+        }
+    }
+
+    calcTotal()
+
+    function getStaticInfo(selector, activeClass) {
+        const elements = document.querySelectorAll(selector)
+
+        elements.forEach(element => {
+            element.addEventListener('click', (event) => {
+                if (event.target.dataset.ratio) {
+                    ratio = +event.target.dataset.ratio
+                    localStorage.setItem('ratio', ratio)
+                }
+                else {
+                    sex = event.target.getAttribute('id')
+                    localStorage.setItem('sex', sex)
+                }
     
+                elements.forEach(element => {
+                    element.classList.remove(activeClass)
+                })
+                
+                event.target.classList.add(activeClass)
+    
+                calcTotal()
+            })
+        }) 
+    }
+    
+    getStaticInfo('#gender div', 'calculating__choose-item_active')
+    getStaticInfo('.calculating__choose_big div', 'calculating__choose-item_active')
+
+    function getDynamicInfo(parentSelector) {
+        const input = document.querySelector(parentSelector)
+        
+
+        input.addEventListener('input', () => {
+
+            if (input.value.match(/\D/g)) {
+                input.style.border = '1px solid red'
+            } else {
+                input.style.border = 'none'
+            }
+
+            switch (input.getAttribute('id')) { 
+                case 'height':
+                    height = +input.value
+                    break
+                case 'weight':
+                    weight = +input.value
+                    break
+                case 'age':
+                    age = +input.value
+                    break
+            }
+            calcTotal()
+        })
+        
+
+       
+    }
+    
+    getDynamicInfo('#height')
+    getDynamicInfo('#weight')
+    getDynamicInfo('#age')  
 })
